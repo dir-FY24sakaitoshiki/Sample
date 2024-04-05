4b1ec567468d3b718dfc07a67ce2da231e6661b1
dir-FY24sakaitoshiki <toshiki.sakai.k321@dirbato.co.jp>
2024-04-05 12:29:13 +0900

    練習

--- Sample/src/sample/Test.java
+++ Sample/src/sample/Test.java
@@ -4,9 +4,12 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("Kiramex");
+        /*
         System.out.println("Java");
         System.out.println("学習を始めましょう");
-
+        */
+        //練習//
+        
     }
 
 }
